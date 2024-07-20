ae4de2fec163044446a1503d105fc18d15a789f1
Keith Brosch <keithtbrosch@gmail.com>
2024-07-20 01:38:06 -0400

    feat: add game chips beneath team cards

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,13 +1,17 @@
 import './GameCard.css';
+import GameChip from '../GameChip/GameChip';
 
 const GameCard = ({gameInfo, setGameFilter}) => {
   return (
-    <div className="game-card" onClick={() => setGameFilter(gameInfo.id)} style={{backgroundImage: `url(${gameInfo.icon})`}}>
-      <div className="game-name">
-        <span>
-          {gameInfo.name.replaceAll('-', ' ')}
-        </span>
+    <div className='game-card-container'>
+      <div className="game-card" onClick={() => setGameFilter(gameInfo.id)} style={{backgroundImage: `url(${gameInfo.icon})`}}>
+        <div className="game-name">
+          <span>
+            {gameInfo.name.replaceAll('-', ' ')}
+          </span>
+        </div>
       </div>
+      <GameChip game={gameInfo} />
     </div>
   )
 }
